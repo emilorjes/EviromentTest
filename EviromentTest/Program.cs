ca9eebd7bcce2a526d872490b6df8058a9f0317b
Emil Örjes <emil.orjes@hotmail.com>
2021-11-12 11:18:22 +0100

    Test

--- EviromentTest/Program.cs
+++ EviromentTest/Program.cs
@@ -6,7 +6,7 @@ namespace EviromentTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World test");
         }
     }
 }
